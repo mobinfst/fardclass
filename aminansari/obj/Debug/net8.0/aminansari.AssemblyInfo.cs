588ac7a2a61339b46e026fa043beac8a763ed422
mobinfst <Mobin.fasahat@yahoo.com>
2025-12-28 11:04:44 +0330

    baseentity

--- aminansari/obj/Debug/net8.0/aminansari.AssemblyInfo.cs
+++ aminansari/obj/Debug/net8.0/aminansari.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aminansari")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f654bbdfc087fc2195ecbeedef66ddcc6e65ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("aminansari")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aminansari")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
